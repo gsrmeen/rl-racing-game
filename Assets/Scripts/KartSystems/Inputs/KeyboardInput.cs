1ea78f5504f292f9f9cdefdd96d5be8f96f82734
gsrmeen <patryk.gsrmeen@gmail.com>
2020-11-04 17:03:42 +0100

    Tweak scene camera

--- Assets/Scripts/KartSystems/Inputs/KeyboardInput.cs
+++ Assets/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -1,16 +1,15 @@
 ﻿using UnityEngine;
 
-namespace KartGame.KartSystems {
-
+namespace KartGame.KartSystems
+{
     public class KeyboardInput : BaseInput
     {
-        public string Horizontal = "Horizontal";
-        public string Vertical = "Vertical";
-
-        public override Vector2 GenerateInput() {
-            return new Vector2 {
-                x = Input.GetAxis(Horizontal),
-                y = Input.GetAxis(Vertical)
+        public override Vector2 GenerateInput()
+        {
+            return new Vector2
+            {
+                x = Input.GetAxis("Horizontal"),
+                y = Input.GetAxis("Vertical")
             };
         }
     }
