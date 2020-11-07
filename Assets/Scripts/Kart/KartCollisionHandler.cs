12198b90c652026c00882edb8277b6af364e13d5
gsrmeen <patryk.gsrmeen@gmail.com>
2020-11-07 16:41:24 +0100

    Add collision handling

---
+++ Assets/Scripts/Kart/KartCollisionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class KartCollisionHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject _mainCollider;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.contacts.Any(p => p.thisCollider.name == _mainCollider.name))
+        {
+            HandleCollision();
+        }
+    }
+
+    private void HandleCollision()
+    {
+        gameObject.SetActive(false);
+    }
+}
